67484e3d179f00c9d6a35ae77f81c04f76a7a177
Ian-Carneiro <ianteixeiraaraujo@gmail.com>
2018-09-14 07:00:50 -0300

    atualização no App

--- src/com/ifpb/view/App.java
+++ src/com/ifpb/view/App.java
@@ -4,25 +4,24 @@ import com.ifpb.model.*;
 public class App {
 	public static void main(String[] args) {
 		GerenciarFuncionario cf = new GerenciarFuncionario();
-		Funcionario f1 = new Funcionario("111.111.111.11", "Zé", "@gmail.com", "telefone", "jfjhkk", "kjghkldff", "1111-1",
-				new ContaBancaria("zxc", 1, "zcxv", true), new Endereco("asddasd", "sdfsdf", "sdsdf", 1, "sdfsf"));
+		Funcionario f1 = new Funcionario("", "", "", "", "", "", "1111-1",
+				new ContaBancaria("", 1, "", true), new Endereco("", "", "", 1, ""));
 		
-		Funcionario f2 = new Funcionario("111.111.111.11", "Zé", "@gmail.com", "telefone", "jfjhkk", "kjghkldff", "1111-1",
-				new ContaBancaria("zxc", 1, "zcxv", true), new Endereco("asddasd", "sdfsdf", "sdsdf", 1, "sdfsf"));
+		Funcionario f2 = new Funcionario("", "", "", "", "", "", "1111-1",
+				new ContaBancaria("", 1, "", true), new Endereco("", "", "", 1, ""));
 		
-		Funcionario f3 = new Funcionario("111.111.111.11", "Zé", "@gmail.com", "telefone", "jfjhkk", "kjghkldff", "1111-2",
-				new ContaBancaria("zxc", 1, "zcxv", true), new Endereco("asddasd", "sdfsdf", "sdsdf", 1, "sdfsf"));
+		Funcionario f3 = new Funcionario("", "", "", "", "", "", "1111-2",
+				new ContaBancaria("", 1, "", true), new Endereco("", "", "", 1, ""));
 		
-		Funcionario f2Editado = new Funcionario("111.111.111.11", "Zé", "@gmail.com", "telefone", "jfjhkk", "kjghkldff", "1111-4",
-				new ContaBancaria("zxc", 1, "zcxv", true), new Endereco("asddasd", "sdfsdf", "sdsdf", 1, "sdfsf"));
+		Funcionario f4 = new Funcionario("", "", "", "", "", "", "1111-4",
+				new ContaBancaria("", 1, "", true), new Endereco("", "", "", 1, ""));
 		
 		System.out.println(cf.addFuncionario(f2));
 		System.out.println(cf.addFuncionario(f1));
 		cf.addFuncionario(f3);
 		System.out.println(cf.buscarFuncionario("1111-1"));
 		System.out.println(cf.removerFuncionario("1111-2"));
-		System.out.println(cf.atualizarFuncionario("1111-1", f2Editado));
-		System.out.println(f2Editado);
+		System.out.println(cf.atualizarFuncionario("1111-1", f4));
 		
 		System.out.println("----------------------------------------------------\n\n\n------------------------------------------------------");
 		
