0dbdae49d20a39d951d3de0a42bede6a584d2577
Hex_cz <hex_cz@hotmail.com>
2018-09-14 09:08:00 -0300

    Criação dos métodos create e read.

--- src/com/ifpb/control/GerenciaCartaoCredito.java
+++ src/com/ifpb/control/GerenciaCartaoCredito.java
@@ -11,9 +11,32 @@ public class GerenciaCartaoCredito {
 		cartoes = new ArrayList<>();
 	}
 	
+	public boolean create(CartaoCredito cartao){
+		if(!cartoes.isEmpty() && cartoes.indexOf(cartao)>=0) {
+			return false;
+		}
+		return cartoes.add(cartao);
+	}
+
+	public CartaoCredito read(String numero){
+		for(CartaoCredito cartao:cartoes) {
+			if(cartao.getNumero().equals(numero)) {
+				return cartao;
+			}
+		}
+		return null;
+	}
+
 	public boolean remove(String numero) {
 		if(!cartoes.isEmpty())
 			return cartoes.remove(read(numero));
 		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "GerenciaCartaoCredito{" +
+				"cartoes=" + cartoes +
+				'}';
 	}
 }
