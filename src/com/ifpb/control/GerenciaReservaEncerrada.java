b8b9e4af05fdc22a11a10819861805615172b717
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2018-09-20 21:06:54 -0300

    adicionando novas funcionalidades GerenciaReservaEncerrada

--- src/com/ifpb/control/GerenciaReservaEncerrada.java
+++ src/com/ifpb/control/GerenciaReservaEncerrada.java
@@ -1,31 +1,42 @@
 package com.ifpb.control;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.ifpb.model.Reserva;
 
 public class GerenciaReservaEncerrada {
-	private static List<Reserva> reservas;
-	
-	public GerenciaReservaEncerrada() {
-		reservas = new ArrayList<>();
-	}
+	private static List<Reserva> reservas = new ArrayList<>();
 	
 	public static boolean add(Reserva reserva) {
-		if(buscarCodigoReserva(reserva.getCodigo())>0)
+		if(buscarIndiceReserva(reserva.getCodigo())>0)
 			return reservas.add(reserva);
 		return false;
 	}
 	
-	public static Reserva buscarReserva(int codigo) {
-		for(int i = 0;i<reservas.size();i++)
-			if(reservas.get(i).getCodigo() == codigo)
-				return reservas.get(i);
-		return null;
+	public static Float ganhoPeriodo(LocalDate inicio, LocalDate Fim) {
+		Float ganho = 0f;
+		for(Reserva r: reservas) {
+			if(r.getDataInicio().equals(inicio) || r.getDataInicio().isAfter(inicio))
+				if(r.getDataFim().equals(Fim) || r.getDataFim().isBefore(Fim))
+					ganho += r.getHospedagem().getValor();
+		}
+		return ganho;
 	}
 	
-	public static int buscarCodigoReserva(int codigo) {
+	public static List<Reserva> reservasPeriodo(LocalDate inicio, LocalDate fim){
+		List<Reserva> reservasPeriodo = new ArrayList<>();
+		for(Reserva r: reservas) {
+			if(r.getDataInicio().equals(inicio) || r.getDataInicio().isAfter(inicio))
+				if(r.getDataFim().equals(fim) || r.getDataFim().isBefore(fim))
+					reservasPeriodo.add(r);
+		}
+		return reservasPeriodo;
+					
+	}
+	
+	public static int buscarIndiceReserva(int codigo) {
 		for(int i = 0;i<reservas.size();i++)
 			if(reservas.get(i).getCodigo() == codigo)
 				return i;
